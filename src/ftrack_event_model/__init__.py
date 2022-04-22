4cd2743c53a40f01825579b561fbd46fa4595ba6
Andy Guo <gxagxagxa@me.com>
2022-04-22 18:29:32 +0800

    add factory function, to construct corresponding event model based on topic

---
+++ src/ftrack_event_model/__init__.py
@@ -0,0 +1,23 @@
+def event_model_factory(topic: str, event):
+    if topic == 'ftrack.update':
+        import ftrack_event_model.update
+        return ftrack_event_model.update.FtrackEventUpdateModel(**event)
+
+    if topic == 'ftrack.meta.connected':
+        import ftrack_event_model.meta.connected
+        return ftrack_event_model.meta.connected.FtrackEventMetaConnectedModel(**event)
+
+    if topic.startswith('ftrack.action'):
+        if topic == 'ftrack.action.discover':
+            import ftrack_event_model.action.discover
+            return ftrack_event_model.action.discover.FtrackEventActionDiscoverModel(**event)
+
+        if topic == 'ftrack.action.launch':
+            import ftrack_event_model.action.launch
+            return ftrack_event_model.action.launch.FtrackEventActionLaunchModel(**event)
+
+        if topic == 'ftrack.action.trigger-user-interface':
+            import ftrack_event_model.action.trigger_user_interface
+            return ftrack_event_model.action.trigger_user_interface.FtrackEventActionTriggerUserInterfaceModel(**event)
+
+    raise AttributeError('topic is not ftrack-defined')
