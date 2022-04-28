5166a1441c03ba9353c7b6b47b6b633fd91a4223
Andy Guo <gxagxagxa@me.com>
2022-04-28 21:41:37 +0800

    add event data reference

---
+++ references/ftrack.action.discover.py
@@ -0,0 +1,15 @@
+{'id': '1fd9b1fec4a1469e80a87f559fd95bf3',
+ 'data': {'selection':
+     [
+         {'entityId': '29ae6f00-c44c-11ec-9ae8-928afa1aad5c', 'entityType': 'show'}],
+     'groupers': [],
+     'filters': [],
+     'sorters': [
+         {'direction': 'ASC', 'property': 'label', 'root': 'data'}]},
+ 'topic': 'ftrack.action.discover',
+ 'sent': None,
+ 'source': {'id': '38807d3ce9c9441f82dd37c09121e658',
+            'user': {'username': 'wuqingjun@qiyi.com',
+                     'id': 'bcdf57b0-acc6-11e1-a554-f23c91df1211'}},
+ 'target': '',
+ 'in_reply_to_event': None}
---
+++ references/ftrack.action.launch.py
@@ -0,0 +1,17 @@
+{'id': 'a567062523504da1b7bcf8ec4f7a76ad',
+ 'data': {'actionIdentifier': 'ftrack-connect-launch-nuke-studio',
+          'applicationIdentifier': 'nuke-studio_13.1v1 [legacy]',
+          'variant': '13.1v1 [legacy]',
+          'integrations': {'legacy': ['ftrack-connect-nuke-studio']},
+          'label': 'Nuke Studio',
+          'host': 'Andy-IQIYI.local',
+          'selection': [
+              {'entityId': '29ae6f00-c44c-11ec-9ae8-928afa1aad5c', 'entityType': 'show'}],
+          'icon': 'https://iqiyi.ftrackapp.cn/application_icons/nuke_studio.png'},
+ 'topic': 'ftrack.action.launch',
+ 'sent': None,
+ 'source': {'id': '38807d3ce9c9441f82dd37c09121e658',
+            'user': {'username': 'wuqingjun@qiyi.com',
+                     'id': 'bcdf57b0-acc6-11e1-a554-f23c91df1211'}},
+ 'target': '',
+ 'in_reply_to_event': None}
